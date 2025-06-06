e576240ae77972dab64d83818b5ff8c8ae573670
imitxtion <imitxtion@proton.me>
2025-06-06 15:09:06 -0700

    Added Activity 1. Completed part 1/2 of the assignment 1.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,6 +16,7 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
